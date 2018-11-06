cac888b2a4a5a49121bc088f39c464ccdf0fd992
wes1ey <sapc87952@gmail.com>
2018-11-06 15:15:36 +0800

    make it better

---
+++ bottender.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  messenger: {
+    appId: '240508576634790',
+    appSecret: '79a7c0757f7c788cdd51f7ec51860b76',
+    accessToken: 'EAADavcRGl6YBACZBYZAhvRZCeq4jstcN3ooUuNxCAMqvNKRiZBcZBlC0i1nZBD9Lc9yiKcyPWfEOhG6XdsSXAlZCpXZAOqTF2eWN5ZCXFLHveuRtO9RwMZBf3iF0qeC9ByZAvZArJe2k3j8tDjoZA9cxrM4AqU0EwPL0zR0jlvwWceOQSrwlPKZBmh41iK',
+    verifyToken: 'citiesocial',
+  },
+};
